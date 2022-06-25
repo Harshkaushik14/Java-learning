63a1f006d9a71dfc7be5a3d7205203838212c2f7
Harshkaushik14 <kaushikharsh86@gmail.com>
2022-06-25 21:38:47 +0530

    initial commit

---
+++ src/NumberHalfPyramid.java
@@ -0,0 +1,19 @@
+public class NumberHalfPyramid {
+    public static void main(String[] args) throws Exception {
+        int n = 4;
+       
+        for (int row = 1; row <= n; row++) {
+       
+           for (int col = 1; col <= row; col++) {
+               System.out.print(col);
+           }
+           System.out.println(" ");
+       
+           
+        }
+       
+           }
+       }
+       
+
+
---
+++ src/NumberInvertedHalfPyramid.java
@@ -0,0 +1,15 @@
+public class NumberInvertedHalfPyramid {
+    public static void main(String[] args) throws Exception {
+        int n = 4;
+
+        for (int row = 1; row <= n; row++) {
+
+            for (int col = 1; col <= n - row + 1; col++) {
+                System.out.print(col);
+            }
+            System.out.println(" ");
+
+        }
+
+    }
+}
